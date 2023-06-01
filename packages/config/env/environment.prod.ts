5bc59836c968b16a9d80e9134344c9b304acd519
Jesús Bintaned <jesus@10labs.es>
2023-06-01 02:04:10 +0200

    Preparado sstema para despliegue

---
+++ packages/config/env/environment.prod.ts
@@ -0,0 +1,4 @@
+export const envVars = {
+  ENVIRONMENT: 'production',
+  API_URL: 'https://master-uned.jebiba.es/malware/api'
+};
--- packages/config/env/environment.test.ts
+++
@@ -1,4 +0,0 @@
-export const envVars = {
-  ENVIRONMENT: 'test',
-  API_URL: 'http://localhost:8000/malware/api'
-};
