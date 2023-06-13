e1ea50e661b7cad99a3b9b21b7db7bb3b1eb40b4
Jesús Bintaned <jesus@10labs.es>
2023-06-14 00:18:46 +0200

    Correccion de errores

--- packages/core/shared/domain/contracts/http.ts
+++ packages/core/shared/domain/contracts/http.ts
@@ -7,31 +7,22 @@ export interface HttpService {
   get<T>(
     url: string,
     params?: Record<string, unknown>,
-    headers?: Record<string, unknown>,
-    hasBearerAuth?: boolean
+    headers?: Record<string, unknown>
   ): Promise<HttpResponse<T>>;
-  post<T>(
-    url: string,
-    data: unknown,
-    headers?: Record<string, unknown>,
-    hasBearerAuth?: boolean
-  ): Promise<HttpResponse<T>>;
+  post<T>(url: string, data: unknown, headers?: Record<string, unknown>): Promise<HttpResponse<T>>;
   put<T>(
     url: string,
     data: Record<string, unknown>,
-    headers?: Record<string, unknown>,
-    hasBearerAuth?: boolean
+    headers?: Record<string, unknown>
   ): Promise<HttpResponse<T>>;
   patch<T>(
     url: string,
     data?: Record<string, unknown>,
-    headers?: Record<string, unknown>,
-    hasBearerAuth?: boolean
+    headers?: Record<string, unknown>
   ): Promise<HttpResponse<T>>;
   delete<T>(
     url: string,
     headers?: Record<string, unknown>,
-    hasBearerAuth?: boolean,
     data?: unknown
   ): Promise<HttpResponse<T>>;
 }
